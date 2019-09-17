2cac8ba81373b729a6bc0b61b0f1309bf48dbc2a
jsolana <javiersolanahuertas@gmail.com>
2019-09-17 15:35:33 +0200

    First version of Elastic stream sink library

---
+++ Streams.Elastic/ElasticStreamSink.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Nest;
+using Polly;
+
+namespace Com.RFranco.Streams.Elastic
+{
+    /// <summary>
+    /// A Kakfa sink, that allows to specify message keys.
+    /// </summary>
+    /// <typeparam name="K">The type of the stream message keys.</typeparam>
+    /// <typeparam name="T">The type of the stream messages.</typeparam>
+    public class ElasticStreamSink<K, T> : IKeyedStreamSink<K, T> where T : class where K : class
+    {
+        private IElasticClient ElasticClient;
+        private IStreamSource sourceToCommit;
+
+        private string Index;
+
+        private System.Timers.Timer dumpTimer;
+
+        private List<Message<T>> DataBuffer;
+
+
+        /// <summary>
+        /// When this timeout expires, the sink will wait for the Kafka producer to send
+        /// all the messages and will commit to the source. The default is 5 seconds.
+        /// </summary>
+        public TimeSpan BulkTimeout { get; set; }
+
+        public event Action<StreamingError> OnError;
+
+        public ElasticStreamSink(IElasticClient elasticClient, string index, TimeSpan bulkTimeoutSeconds, long maxBufferSize = 1000)
+        {
+            this.ElasticClient = elasticClient;
+            this.Index = index;
+            this.BulkTimeout = bulkTimeoutSeconds;
+            this.DataBuffer = new List<Message<T>>();
+            
+            SetupCommitTimer(() =>
+                {
+                    DumpMessages();
+                });
+        }
+
+        public void Dump(IEnumerable<T> stream, CancellationToken cancellationToken)
+        {
+            using (cancellationToken.Register(() => cancellationToken.ThrowIfCancellationRequested()))
+            {
+                foreach (var data in stream.Select(s => new Message<T> { Value = s, Key = Guid.NewGuid().ToString() }))
+                {
+                    lock (DataBuffer)
+                    {
+                        DataBuffer.Add(data);
+                    }
+                }
+            }
+        }
+
+        public void DumpWithKey(IEnumerable<KeyValuePair<K, T>> stream, CancellationToken cancellationToken)
+        {
+            using (cancellationToken.Register(() => cancellationToken.ThrowIfCancellationRequested()))
+            {
+               foreach (var data in stream.Select(s => new Message<T> { Value = s.Value, Key = s.Key.ToString() }))
+                {
+                    lock (DataBuffer)
+                    {
+                        DataBuffer.Add(data);
+                    }
+                }
+            }
+        }        
+
+        private void DumpMessages()
+        {
+            if (DataBuffer.Count() != 0)
+            {
+                lock (DataBuffer)
+                {
+                    Polly.Policy.Handle<Exception>().WaitAndRetryForever(retryAttempt =>
+                    {
+                        var delay = TimeSpan.FromMilliseconds(100 * Math.Pow(2, retryAttempt));
+                        if (delay > TimeSpan.FromSeconds(10))
+                            delay = TimeSpan.FromSeconds(10);
+                        return delay;
+                    })
+                    .Execute(() =>
+                    {
+                        var response = ElasticClient.Bulk(b => b.Index(Index)
+                        .IndexMany(DataBuffer, (descriptor, s) => descriptor.Index(Index).Id(s.Key)));
+
+                        if (!response.IsValid)
+                        {
+                            OnError?.Invoke(new StreamingError { Reason = response.OriginalException.Message, IsFatal = false });
+                            throw response.OriginalException;
+                        }
+
+                        DataBuffer.Clear();
+                    });
+                }
+
+                sourceToCommit?.Commit();
+            }
+        }
+
+
+        public void SetSourceToCommit(IStreamSource source)
+        {
+            this.sourceToCommit = source;
+        }
+
+
+        private void SetupCommitTimer(Action onTimeout)
+        {
+            dumpTimer = new System.Timers.Timer(BulkTimeout.TotalMilliseconds);
+            dumpTimer.Elapsed += (sender, eventArgs) =>
+            {
+                onTimeout.Invoke();
+            };
+            dumpTimer.AutoReset = true;
+            dumpTimer.Enabled = true;
+        }
+
+        //
+        // Resumen:
+        //     Represents a (deserialized) Kafka message.
+        private class Message<TValue>
+        {
+            //
+            // Resumen:
+            //     Gets the message key value (possibly null).
+            public string Key { get; set; }
+            //
+            // Resumen:
+            //     Gets the message value (possibly null).
+            public TValue Value { get; set; }
+        }
+    }
+}
