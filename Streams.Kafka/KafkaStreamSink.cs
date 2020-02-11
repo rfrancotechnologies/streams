aec63ced63df7bd528ce87d96ebb511fd331f09b
jsolana_cr <jsolana@rfranco.com>
2020-02-11 11:51:27 +0100

    Added retry policy when ProduceException is thrown in IProducer.Produce() invocation.

--- Streams.Kafka/KafkaStreamSink.cs
+++ Streams.Kafka/KafkaStreamSink.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 using Confluent.Kafka;
+using Polly;
 
 namespace Com.RFranco.Streams.Kafka
 {
@@ -16,7 +17,7 @@ namespace Com.RFranco.Streams.Kafka
         private readonly ProducerConfig producerConfig;
         private readonly ISerializer<K> keySerializer;
         private readonly ISerializer<T> valueSerializer;
-        private readonly string topic;        
+        private readonly string topic;
         private int CommitTimeoutMs;
 
         public KafkaStreamSink(ProducerConfig producerConfig, string topic, ISerializer<K> keySerializer, ISerializer<T> valueSerializer, int commitTimeoutMs = 5000)
@@ -35,58 +36,90 @@ namespace Com.RFranco.Streams.Kafka
 
         public void Dump(IEnumerable<T> stream, CancellationToken cancellationToken)
         {
-            DumpMessages(stream, value => new Message<K, T>{ Value = value }, cancellationToken);
+            DumpMessages(stream, value => new Message<K, T> { Value = value }, cancellationToken);
         }
 
         public void Dump(IEnumerable<T> stream, Func<T, DateTimeOffset> getMessageDateTime, CancellationToken cancellationToken)
         {
-            DumpMessages(stream, value => new Message<K, T>{ Value = value, 
-                        Timestamp = new Timestamp(getMessageDateTime(value)) }, cancellationToken);
+            DumpMessages(stream, value => new Message<K, T>
+            {
+                Value = value,
+                Timestamp = new Timestamp(getMessageDateTime(value))
+            }, cancellationToken);
         }
 
         public void DumpWithKey(IEnumerable<KeyValuePair<K, T>> stream, CancellationToken cancellationToken)
         {
-            DumpMessages(stream, keyValuePair => new Message<K, T>{ Key = keyValuePair.Key, Value = keyValuePair.Value}, cancellationToken);
+            DumpMessages(stream, keyValuePair => new Message<K, T> { Key = keyValuePair.Key, Value = keyValuePair.Value }, cancellationToken);
         }
 
         public void DumpWithKey(IEnumerable<KeyValuePair<K, T>> stream, Func<T, DateTimeOffset> getMessageDateTime, CancellationToken cancellationToken)
         {
-            DumpMessages(stream, keyValuePair => new Message<K, T>{ Key = keyValuePair.Key, 
-                            Value = keyValuePair.Value, 
-                            Timestamp = new Timestamp(getMessageDateTime(keyValuePair.Value))}, cancellationToken);
-        }                
+            DumpMessages(stream, keyValuePair => new Message<K, T>
+            {
+                Key = keyValuePair.Key,
+                Value = keyValuePair.Value,
+                Timestamp = new Timestamp(getMessageDateTime(keyValuePair.Value))
+            }, cancellationToken);
+        }
 
         private void DumpMessages<M>(IEnumerable<M> messages, Func<M, Message<K, T>> getMessage, CancellationToken cancellationToken)
         {
             var producerBuilder = new ProducerBuilder<K, T>(producerConfig);
             producerBuilder.SetKeySerializer(keySerializer);
             producerBuilder.SetValueSerializer(valueSerializer);
-            producerBuilder.SetErrorHandler((_, e) => OnError?.Invoke(new StreamingError{ IsFatal = e.IsFatal, Reason = e.Reason }));
+            producerBuilder.SetErrorHandler((_, e) => OnError?.Invoke(new StreamingError { IsFatal = e.IsFatal, Reason = e.Reason }));
             producerBuilder.SetStatisticsHandler((_, statistics) => OnStatistics?.Invoke(statistics));
 
             Stopwatch processTime = Stopwatch.StartNew();
-            
+
             using (var p = producerBuilder.Build())
             {
-                foreach(M message in messages)
+                foreach (M message in messages)
                 {
-                    if (cancellationToken.IsCancellationRequested) 
-                        break;                    
-                    
-                    p.Produce(this.topic, getMessage.Invoke(message), 
-                    r => {
-                        if (r.Error.IsError)
-                            OnError?.Invoke(new StreamingError{ IsFatal = r.Error.IsFatal, Reason = r.Error.Reason });
+                    if (cancellationToken.IsCancellationRequested)
+                        break;
+
+                    Policy.Handle<ProduceException<K, T>>()
+                    .WaitAndRetryForever(retryAttempt => TimeSpan.FromMilliseconds(Math.Min(100 * Math.Pow(2, retryAttempt), 10000)),
+                    (exception, timespan) =>
+                    {
+                        var kafkaException = exception as ProduceException<K, T>;
+                        OnError?.Invoke(new StreamingError
+                        {
+                            IsFatal = kafkaException.Error.IsFatal,
+                            Reason = $"{kafkaException.Error.Reason}. The message with key {kafkaException.DeliveryResult.Key} in topic {kafkaException.DeliveryResult.Topic} will be resent on {timespan.TotalMilliseconds} ms."
+                        });
+                    })
+                    .Execute(() =>
+                    {
+                        if (!cancellationToken.IsCancellationRequested)
+                        {
+                            p.Produce(this.topic, getMessage.Invoke(message),
+                            r =>
+                            {
+                                if (r.Error.IsError)
+                                    OnError?.Invoke(new StreamingError { IsFatal = r.Error.IsFatal, Reason = r.Error.Reason });
+                            });
+                        }
                     });
 
                     if (processTime.ElapsedMilliseconds >= CommitTimeoutMs)
                     {
-                        p.Flush(cancellationToken);                        
-                        OnCommit?.Invoke();
-                        processTime.Restart();
-                    } 
+                        p.Flush(cancellationToken);
+                        if (!cancellationToken.IsCancellationRequested)
+                        {
+                            OnCommit?.Invoke();
+                            processTime.Restart();
+                        }
+                    }
                 }
+
+                p.Flush(cancellationToken);
+
+                if (!cancellationToken.IsCancellationRequested)
+                    OnCommit?.Invoke();
             }
-        }        
+        }
     }
 }
